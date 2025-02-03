471863f195dedb1803cc0ad09c88fbafd2a2c64a
DenisPepper <thech@yandex.ru>
2025-02-03 18:03:56 +0300

    реорганизация кода

--- src/samples/typed-object/TypedObject.tsx
+++ src/samples/typed-object/TypedObject.tsx
@@ -1,29 +1,5 @@
 import './TypedObject.module.css';
-
-// const user = { name: 'Denis', age: 42, js: true };
-// type User = typeof user;
-// type UserKeys = keyof User;
-type Method = (a: number, b: number) => number;
-
-// satisfies сохранит удобство первой реализации
-// https://www.freecodecamp.org/news/typescript-satisfies-operator/
-/* 
-type Keys = "personID" | "personName" | "personEmail" | "personAge" | "personPhone";
-
-const person = {
-  personID: 12345,
-  personName: "Jacky",
-  personEmail: "jacky@testing.com",
-  personAge: 22,
-} satisfies Partial<Record<Keys, string | number>>;
-*/
-export const methods = {
-  '+': (a, b) => a + b,
-  '-': (a, b) => a - b,
-  '*': (a, b) => a * b,
-  '/': (a, b) => a / b,
-  '**': (a, b) => a ** b,
-} satisfies Record<string, Method>;
+import { methods } from './methods';
 
 type TypedObjectProps = {
   a?: number;
